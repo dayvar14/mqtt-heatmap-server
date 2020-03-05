cdef9a632d1af7c49c9fcaf51f1b08b4cf30aaef
Daniel Ayvar <dayvar@neiu.edu>
2020-03-04 18:29:20 -0600

    added more comments

--- test-client/publisher.js
+++ test-client/publisher.js
@@ -6,23 +6,30 @@ const mqttClient = mqtt.connect(process.env.MQTT_BROKER_HOST || 'localhost',{por
 const topic = process.env.COORDINATES_SUBSCRIBE_TOPIC;
 const delay = process.env.COORDINATES_PUBLISH_DELAY || 2000;
 
-
-var usernames = ['test_username_1', 'test_username_2', 'test_username-3']
+var base = "test_username"
 console.log(process.env.MQTT_BROKER_HOST)
 async function init() {
+  let users = []
+
+  for(var i = 0; i < 100;i++){
+    let lat = (Math.random() * (41.982887 - 41.975692) + 41.975692).toFixed(6);
+    let lng = (Math.random() * (-87.716022 - -87.720914) + -87.7209142).toFixed(6);
+    let id = base + i;
+    users.push( {lat:lat, lng:lng, id:id});
+  }
+  
     while (true) {
-      for(var i = 0; i < usernames.length;i++){
-        mqttClient.publish(topic,'{"id": "dayvar", "lat":"41.98288","lng":"-87.72090"}');
+      for(var i = 0; i < 100;i++){
+        if( Math.random()
+        //{id:<id>, lat:<latitude>, lng:<longitude>}
+        let object = {lat:lat, lng:lng, id:id}
+        mqttClient.publish(topic,JSON.stringify(object));
       }
+      console.log("Published coordinates on " + topic + "...")
+        await sleep(3000);
         
-        await sleep(delay);
-        console.log("Published coordinates on " + topic + "...")
     }
 }
-console.log(Number.parseFloat("41.982887").toFixed(process.env.COORDINATES_LAT_PRECISION))
-console.log(Number.parseFloat("-87.720914").toFixed(process.env.COORDINATES_LAT_PRECISION))
-console.log(process.env.MIN_COORDINATES_LAT);
-console.log(process.env.MAX_COORDINATES_LAT);
 
 function sleep(ms) {
   return new Promise((resolve) => {
